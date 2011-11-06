ed0db544ffe1b7024502c41934f1aa1936a6084d
Jon Olav Vik <jonovik@gmail.com>
2011-11-06 02:27:50 +0100

    Factor out technical tests for module "unstruct".

---
+++ cgp/test/test_unstruct.py
@@ -0,0 +1,63 @@
+"""Tests for :mod:`cgp.utils.unstruct`."""
+
+import numpy as np
+from cgp.utils.unstruct import unstruct
+
+def test_dimensions():
+    """Check relationship between dimensions of x and unstruct(x)."""
+    # x has 2 fields and dimensions (3, 4)
+    x = np.arange(24).view(dtype=[(i, int) for i in "ab"]).reshape(3, 4)
+    # u has dimensions (3, 4, 2)
+    u = unstruct(x)
+    # change to u affects x
+    u[:] += 10
+    # Last dimension of u corresponds to fields of x
+    np.testing.assert_equal(x["a"], u[..., 0])
+    # First dimensions of u corresponds to first dimensions of x
+    np.testing.assert_equal(x[0][0].item(), u[0][0])
+
+def test_dtypes():
+    """Verify that unstruct() handles different dtypes correctly."""
+    for fieldtype in np.int8, np.int32, float:
+        dtype = [(i, fieldtype) for i in "ab"]
+        x = np.arange(4, dtype=fieldtype).view(dtype)
+        yield np.testing.assert_equal, unstruct(x), [[0, 1], [2, 3]]
+
+def test_zero_rank():
+    """
+    Handle intricacies of zero-rank arrays.
+    
+    `Zero-rank arrays 
+    <http://projects.scipy.org/numpy/wiki/ZeroRankArray>`_ 
+    are tricky; they can be structured, yet be of type numpy.void.
+
+    Here, x0 and x1 are almost, but not completely, the same:
+    
+    >>> fieldtype = np.int32   # ensure same result on 32- and 64-bit platforms
+    >>> dtype = [("a", fieldtype), ("b", fieldtype)]
+    >>> x0 = np.array([(0, 1)], dtype=dtype)[0]
+    >>> x1 = np.array((0, 1), dtype=dtype)
+
+    Despite a lot of equalities below, x0 and x1 are of different type.
+    
+    >>> (x0 == x1) and (x0.shape == x1.shape) and (x0.dtype == x1.dtype)
+    True
+    >>> x0
+    (0, 1)
+    >>> x1
+    array((0, 1), dtype=[('a', '<i4'), ('b', '<i4')])
+    >>> type(x0), type(x1)
+    (<type 'numpy.void'>, <type 'numpy.ndarray'>)
+    
+    Unstructuring them was tricky, but finally works.
+    
+    >>> unstruct(x0)
+    array([0, 1])
+    >>> unstruct(x1)
+    array([0, 1])
+    """
+    fieldtype = np.int32   # ensure same result on 32- and 64-bit platforms
+    dtype = [("a", fieldtype), ("b", fieldtype)]
+    x0 = np.array([(0, 1)], dtype=dtype)[0]
+    x1 = np.array((0, 1), dtype=dtype)
+    np.testing.assert_equal(unstruct(x0), unstruct(x1))
--- cgp/utils/unstruct.py
+++ cgp/utils/unstruct.py
@@ -1,5 +1,5 @@
 """
-Unstructured view of a structured array whose fields are all the same type.
+Unstructured view of a structured array whose fields are all of the same type.
 """
 
 import numpy as np
@@ -8,64 +8,39 @@ def unstruct(x):
     """
     Unstructured view of a structured array whose fields are all the same type.
     
-    Usage:
+    :param array_like x: Array, possibly with named fields.
+    :return: Array with new dimension(s) appended for fields.
     
-    >>> fieldtype = np.int32   # ensure same result on 32- and 64-bit platforms
-    >>> dtype = [("a", fieldtype), ("b", fieldtype)]
-    >>> x = np.arange(2, dtype=fieldtype).view(dtype)
-    >>> x
-    array([(0, 1)], dtype=[('a', '<i4'), ('b', '<i4')])
-    >>> unstruct(x) == np.array([[0, 1]])
-    array([[ True,  True]], dtype=bool)
+    Example: Unstructuring a 1-D array with two named fields.
     
-    Details:
+    >>> x = np.array([(0, 1), (2, 3), (4, 5)], dtype=[('a', '<i4'), ('b', '<i4')])
+    >>> u = unstruct(x)
+    >>> u
+    array([[0, 1], [2, 3], [4, 5]])
+    
+    The unstructured array has an extra dimension appended, with a length 
+    equal to the number of fields in *x*.
     
     >>> x.shape
-    (1,)
-    >>> unstruct(x).shape
-    (1, 2)
-    >>> x = np.arange(4, dtype=fieldtype).view(dtype)
+    (3,)
+    >>> u.shape
+    (3, 2)
     
-    Assignment to u will also affect x.
+    The result is a view of the original array, so modifications to *u* will 
+    also affect *x*.
     
-    >>> u = unstruct(x)
-    >>> u[:] = 2
+    >>> u.fill(2)
     >>> x
-    array([(2, 2), (2, 2)], dtype=[('a', '<i4'), ('b', '<i4')])    
-    
-    (Some examples below use ELLIPSIS to allow for 64-bit Numpy tacking 
-    ", dtype=np.int32" onto the output.)
-    
-    >>> x, unstruct(x), x.shape, unstruct(x).shape # doctest: +ELLIPSIS
-    (array([(0, 1), (2, 3)], dtype=...), array([[0, 1], [2, 3]]...), (2,), (2, 2))
-    >>> x = np.arange(24, dtype=fieldtype).view(dtype).reshape(4, 3)
-    >>> x
-    array([[(0, 1), (2, 3), (4, 5)],
-           [(6, 7), (8, 9), (10, 11)],
-           [(12, 13), (14, 15), (16, 17)],
-           [(18, 19), (20, 21), (22, 23)]],
-          dtype=[('a', '<i4'), ('b', '<i4')])
-    >>> unstruct(x) # doctest: +ELLIPSIS
-    array([[[ 0,  1], [ 2,  3], [ 4,  5]],
-           [[ 6,  7], [ 8,  9], [10, 11]],
-           [[12, 13], [14, 15], [16, 17]],    
-           [[18, 19], [20, 21], [22, 23]]]...)
-    >>> unstruct(x[0]) # doctest: +ELLIPSIS
-    array([[0, 1], [2, 3], [4, 5]]...)
+    array([(2, 2), (2, 2), (2, 2)], dtype=[('a', '<i4'), ('b', '<i4')])
     
     Already unstructured types pass right through.
     
     >>> unstruct(np.arange(2))
     array([0, 1])
     
-    Float types.
-    
-    >>> dtype = [("a", float), ("b", float)]
-    >>> unstruct(np.arange(4.0).view(dtype))
-    array([[ 0.,  1.], [ 2.,  3.]])
-    
     Nonscalar fields are OK if all have the same shape.
-    The result of unstruct(x) has shape (len(x), len(x.dtype), fieldshape).
+    The result of ``unstruct(x)`` has shape 
+    ``(len(x), len(x.dtype), fieldshape)``.
     In this example, there are two records with three fields of shape four.
     
     >>> dtype = [("a", np.int32, 4), ("b", np.int32, 4), ("c", np.int32, 4)]
@@ -89,40 +64,13 @@ def unstruct(x):
     AssertionError: One or more fields has a different type or shape 
     than the first: [('a', '|i1'), ('b', '<i2')]
     
-    Non-array objects are converted with np.array() if possible.
+    Non-array objects are converted with np.array() if possible. 
+    List of arrays are OK if they can be concatenated.
     
     >>> unstruct(range(5))
     array([0, 1, 2, 3, 4])
-    
-    List of arrays are OK if they can be concatenated.
-    
-    >>> unstruct([x[0,0], x[0,1]])                          # doctest: +ELLIPSIS
+    >>> unstruct([np.array([0, 1]), np.array([2, 3])])
     array([[0, 1], [2, 3]])
-    
-    Zero-rank arrays are tricky; they can be structured, yet of type numpy.void.
-    http://projects.scipy.org/numpy/wiki/ZeroRankArray
-    Here, x0 and x1 are almost, but not completely, the same:
-    
-    >>> x0 = x[0, 0]
-    >>> x1 = np.array((0, 1), dtype=[("a", fieldtype), ("b", fieldtype)])
-    >>> (x0 == x1) and (x0.shape == x1.shape) and (x0.dtype == x1.dtype)
-    True
-    
-    In spite of the equalities above, x0 and x1 are of different type.
-    
-    >>> x0
-    (0, 1)
-    >>> x1                                                  # doctest: +ELLIPSIS
-    array((0, 1), dtype=[('a', '<i4'), ('b', '<i4')])
-    >>> type(x0), type(x1)
-    (<type 'numpy.void'>, <type 'numpy.ndarray'>)
-    
-    Unstructuring them was tricky, but finally works.
-    
-    >>> unstruct(x0)
-    array([0, 1])
-    >>> unstruct(x1)
-    array([0, 1])
     """
     x = np.asanyarray(x)
     if x.dtype == object:
@@ -142,8 +90,7 @@ def unstruct(x):
     # So promote to 1d once we're done computing the new shape.
     x = np.atleast_1d(x) # avoid ValueError
     xv = x.view(fieldtype)  # pylint: disable=E1103
-    xv.shape = shape
-    return xv
+    return xv.reshape(shape)
 
 if __name__ == "__main__":
     import doctest
