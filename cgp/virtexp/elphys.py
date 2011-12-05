987defb2e150b10e122389881dc48251ceb23e3d
Jon Olav Vik <jonovik@gmail.com>
2011-12-05 17:44:17 +0100

    More docs for class Paceable; Bond example. Builds and tests OK.

--- cgp/virtexp/elphys.py
+++ cgp/virtexp/elphys.py
@@ -1,41 +1,67 @@
 """
 Virtual experiments for cellular electrophysiology.
 
-Assumed interface:
+These protocols assume that the :wiki:`transmembrane potential` is a variable 
+named *V* in the model. (If the transmembrane potential is named differently, 
+use the *rename* argument to the 
+:meth:`~cgp.physmod.cellmlmodel.Cellmlmodel` constructor.)
 
-* :data:`V` : Transmembrane voltage
-* :data:`i_stim` : Stimulus current
+Many models of cellular electrophysiology, such as the 
+:cellml:`Bondarenko 
+<11df840d0150d34c9716cd4cbdd164c8/bondarenko_szigeti_bett_kim_rasmusson_2004_apical>`
+and 
+:cellml:`Ten Tusscher 
+<e946a72663bdf17ef6752980a0232351/tentusscher_noble_noble_panfilov_2004_a>` 
+models, have hardcoded a protocol of regular
+pacing, which must be disabled to apply other protocols such as regular pacing 
+(:doi:`Cooper et al. 2011 <10.1016/j.pbiomolbio.2011.06.003>`).
+
+Here, regular pacing is assumed to be governed by the following parameters:
+
+* *stim_amplitude* : Magnitude of the stimulus current.
+* *stim_period* : Interval between the beginnings of successive stimuli.
+
+Some models have a parameter called *stim_start*, running the model unpaced 
+for some time before the stimulus starts. Here it is assumed that any such 
+delay is set to zero.
+
+The implementation of voltage clamp experiments assumes that the model object 
+has a 
+:meth:`~cgp.cvodeint.namedcvodeint.Namedcvodeint.clamp` method.
 """
+
+import numpy as np
+from pysundials import cvode
+
+from ..physmod.cellmlmodel import Cellmlmodel
+from . import ap_stats
 
 class Paceable(object):
     """
     Mixin class for pacing electrophysiological models.
     
-    Applicable to any model with a transmembrane potential and stimulus 
-    current.
+    This protocol is applicable to any model with a transmembrane potential 
+    *V* and stimulus parameters *stim_amplitude* and *stim_period*.
     
     To use, define a class inheriting both from :class:`Paceable` and 
-    :class:`~cellmlmodels.cellmlmodel.Cellmlmodel`. You may wish to specify 
-    the model identifier (*exposure_workspace*) as a default argument to the 
-    constructor.
-    
-    >>> exposure_workspace = "11df840d0150d34c9716cd4cbdd164c8/"
-            "bondarenko_szigeti_bett_kim_rasmusson_2004_apical"
-    >>> class Bond(Namedcvodeint, Paceable):
-    ...     pass
-    >>> bond = Bond(exposure_workspace)
-    >>> t, y, stats = bond.ap()
-    >>> plt.plot(t, y.V)
+    :class:`~cgp.physmod.cellmlmodel.Cellmlmodel`. See the source code for 
+    class :class:`Bond` for an example, including defining the model 
+    identifier as the default *exposure_workspace* for the class constructor.
     """
     
-        def ap_plain(self, y=None, pr=None, p_repol=r_[0.25, 0.5, 0.75, 0.9], ignore_flags=False):
+    def ap(self, y=None, pr=None, p_repol=(0.25, 0.5, 0.75, 0.9), 
+        ignore_flags=False):
         """
-        Simulate action potential triggered by stimulus current. No rootfinding.
+        Simulate action potential triggered by stimulus current.
         
-        Arguments are as for :meth:`ap`.
+        :param array_like y: initial state
+        :param array_like p_repol: compute action potential duration to 
+           :math:`p_{repol} \times 100\%` repolarization
+        :param bool ignore_flags: disable sanity checking of the shape of the 
+           action potential
         
-        >>> bond = Bond()
-        >>> t, y, stats = bond.ap_plain()
+        >>> cell = Bond()
+        >>> t, y, stats = cell.ap()
         >>> from pprint import pprint # platform-independent order of dict items
         >>> pprint(stats)
         {'amp': 115.44...,
@@ -50,8 +76,8 @@ class Paceable(object):
         :meth:`~cvodeint.namedcvodeint.Namedcvodeint.autorestore` 
         instead to temporarily modify initial state or parameters.
         
-        >>> with bond.autorestore(V=-60, stim_period=50):
-        ...     t, y, stats = bond.ap_plain()
+        >>> with cell.autorestore(V=-60, stim_period=50):
+        ...     t, y, stats = cell.ap()
         >>> t[-1]
         50.0
         >>> pprint(stats)
@@ -74,17 +100,20 @@ class Paceable(object):
 
         # integrate over stimulus
         # logging.debug("integrate over stimulus")
-        result.append(self.integrate(t=[0, self.pr.stim_duration], y=y, nrtfn=0,  
-            assert_flag=cvode.CV_TSTOP_RETURN, ignore_flags=ignore_flags))
+        result.append(self.integrate(t=[0, self.pr.stim_duration], y=y, 
+            nrtfn=0, assert_flag=cvode.CV_TSTOP_RETURN, 
+            ignore_flags=ignore_flags))
         
         # integrate from the end of this stimulus to the start of the next one:
         # logging.debug("integrate from end of stimulus to start of next")
         # RHS discontinuity here, so force the solver to re-initialize: len(t)>1
-        result.append(self.integrate(t=[self.pr.stim_duration, self.pr.stim_period], nrtfn=0, 
-            assert_flag=cvode.CV_TSTOP_RETURN, ignore_flags=ignore_flags))
+        result.append(self.integrate(
+            t=[self.pr.stim_duration, self.pr.stim_period], 
+            nrtfn=0, assert_flag=cvode.CV_TSTOP_RETURN, 
+            ignore_flags=ignore_flags))
         
         # logging.debug("integration finished")
-        t, Y, flag = zip(*result) # (t, Y, flag), where each is a tuple
+        t, Y, _flag = zip(*result) # (t, Y, flag), where each is a tuple
         
         # The items of the tuples refer to these intervals
         # 0) stimulus
@@ -104,3 +133,49 @@ class Paceable(object):
         except AttributeError: # model may not have a state variable called Cai
             pass
         return t, Y, stats
+
+class Bond(Cellmlmodel, Paceable):
+    """
+    Example for class :class:`Paceable` using the Bondarenko et al. 2004 model.
+    
+    Please **see the source code** for how this class uses the 
+    :class:`Paceable` mixin to add an experimental protocol to a 
+    :class:`~cgp.physmod.cellmlmodels.Cellmlmodel`.
+    
+    ..  inheritance-diagram: cgp.physmod.cellmlmodel.Cellmlmodel Paceable Bond
+        parts: 1
+    
+    .. todo:: Add voltage clamping.
+    
+    As a convenience feature, the :meth:`Bond` constructor defines the 
+    *exposure_workspace* model identifier as a default argument.
+    Another typical adjustment is to set the redundant parameter *stim_start* 
+    to zero.
+    
+    Once defined, the :class:`Bond` class can be used as follows:
+    
+    ..  plot::
+        :include-source:
+        :width: 300
+        
+        from cgp.virtexp.elphys import Bond
+        bond = Bond()
+        t, y, stats = bond.ap()
+        plt.plot(t, y.V)
+    
+    References:
+    
+    * :doi:`original paper <10.1152/ajpheart.00185.2003>`
+    * :cellml:`CellML implementation 
+      <11df840d0150d34c9716cd4cbdd164c8/bondarenko_szigeti_bett_kim_rasmusson_2004_apical>`
+    """
+    
+    _exposure_workspace = ("11df840d0150d34c9716cd4cbdd164c8/" +
+        "bondarenko_szigeti_bett_kim_rasmusson_2004_apical")
+    
+    def __init__(self, exposure_workspace=_exposure_workspace, 
+                 *args, **kwargs):
+        """Constructor for the :class:`Bond` class."""
+        super(Bond, self).__init__(exposure_workspace, *args, **kwargs)
+        if "stim_start" in self.dtype.p.names:
+            self.pr.stim_start = 0.0
