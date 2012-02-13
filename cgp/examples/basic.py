9c4dab998994b06281f024debca86bbb6c3fa10e
Jon Olav Vik <jonovik@gmail.com>
2012-02-13 22:17:00 +0100

    Merge docstring from simple.py into basic.py. Fix record unpacking.

--- cgp/examples/basic.py
+++ cgp/examples/basic.py
@@ -1,20 +1,77 @@
 """
 Basic example of cGP study of the FitzHugh-Nagumo model of nerve membrane.
 
-This uses the version in FitzHugh (1969). Default parameter values are as in
-figure 3-2 of that paper.
-
 .. plot::
     :width: 400
     :include-source:
-    
-    from cgp.examples.simple import *
+        
+    from cgp.examples.basic import fitzhugh
     import scipy.integrate
     
     t = np.arange(0, 50, 0.1)
     y = scipy.integrate.odeint(fitzhugh, [-0.6, -0.6], t)
     plt.plot(t, y)
     plt.legend(["V (transmembrane potential)", "W (recovery variable)"])
+
+..  plot::
+    
+    from cgp.examples.basic import cgpstudy
+    cgpstudy()
+
+The code in this file is ready to run, but is primarily written to be *read*.
+It exemplifies the building blocks of a cGP model study:
+
+..  list-table::
+    :header-rows: 1
+
+    * * Component
+      * Description
+      * Example
+    * * Generation of genotypes
+      * Genotypic variation whose phenotypic effects are to be estimated.
+      * Full enumeration feasible if the genotype space is small.
+        Other options include pedigrees or statistical experimental designs.
+    * * Genotype-to-parameter map
+      * Lumping together lower-level physiology such as gene regulation
+      * Maximum conductances of ion channels
+    * * Parameter-to-phenotype map
+      * Physiological model
+      * Heart cell electrophysiology
+    * * Virtual experiment
+      * Manipulating the physiological model
+      * Pacing with electrical stimuli
+    * * Aggregate phenotypes
+      * Summarizing the raw model output
+      * Action potential duration
+    * * Analysis, summary, visualization
+      *
+      *
+
+This very simple example defines most components in one file for illustration. 
+Real applications will typically draw upon existing code libraries, model 
+repositories and databases. Linking these together may require a lot of code, 
+a complication which we ignore here to make the main concepts stand out more
+clearly.
+
+Our example system is the :func:`FitzHugh-Nagumo model <fitzhugh>` of an 
+excitable heart muscle cell. It has four parameters, one of which represents 
+a stimulus current. Virtual pacing of the cell can be implemented by setting 
+the stimulus current to a nonzero value at regular intervals. The resulting 
+"action potential" (time-course of transmembrane voltage) is often 
+characterized by its duration, for instance APD90 for the action potential 
+duration to 90 % repolarization.
+
+Here, we assume that each parameter is governed by a single gene of which 
+there are two alleles. This trivial genotype-to-parameter map is a caricature 
+in the absence of actual data, but is a minimal assumption to make the model 
+amenable to cGP analysis.
+
+Here we can compute phenotypes for all 27 genotypes (three loci with three 
+possibilities each gives 
+aa bb cc, aa bb Cc, aa bb CC, aa Bb cc, ..., AA BB CC). In higher-dimensional 
+cases, the genotype space must be sampled according to some experimental 
+design, possibly constrained by pedigree information.
+
 
 References:
 
@@ -72,7 +129,7 @@ def fitzhugh(y, t=None, par=par0):  # pylint: disable=W0613
     .. seealso:: :class:`cgp.virtexp.examples.Fitz`.
     """
     V, W = y
-    a, b, theta, I = par
+    a, b, theta, I = par.item()
     Vdot = V - V * V * V / 3.0 - W + I # Eq. (3-6) of FitzHugh (1969)
     Wdot = theta * (V + a - b * W)     # Eq. (3-6) of FitzHugh (1969)
     return Vdot, Wdot
@@ -86,7 +143,7 @@ def par2ph(par):
     y0 = np.rec.array([(-0.5, -0.5)], dtype=[("V", float), ("W", float)])
     t = np.rec.fromarrays([np.arange(0, 1000, 0.1)], names="t")
     # Use .item() to convert between named and plain arrays
-    y = scipy.integrate.odeint(fitzhugh, y0.item(), t.t, args=(par.item(),))
+    y = scipy.integrate.odeint(fitzhugh, y0.item(), t.t, args=(par,))
     # Combine t and fields of y into one recarray with array-valued fields
     return restruct(cbind(t, y.view(y0.dtype)))
 
