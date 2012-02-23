4c8b9125170f04075e93915c54684ea9ab8dc711
Jon Olav Vik <jonovik@gmail.com>
2012-02-23 06:15:13 +0100

    Clean up example in sensitivity_beeler.py.
    Todo: Perhaps add figure to Sphinx docs. Could "def main(fields):" and
    show just a couple of phenotypes.

--- cgp/examples/sensitivity_beeler.py
+++ cgp/examples/sensitivity_beeler.py
@@ -1,58 +1,52 @@
 """Sensitivity analysis of action potential duration, bridging R and Python."""
 # pylint: disable=C0301, R
 
-import logging
-
+# Numerics
 import numpy as np
-#from matplotlib import pyplot as plt
-#from IPython.parallel import Client
+# Bridge to the R statistical software
 import rpy2.rinterface as ri
+from cgp.rnumpy.rnumpy import r, py2ri
+# Caching and on-demand recomputing
 from joblib import Memory
 
+# Wrapping ODE solver for CellML so it knows about variable names, etc.
 from cgp.physmod.cellmlmodel import Cellmlmodel
+# Virtual experiments
 from cgp.phenotyping.attractor import AttractorMixin
 from cgp.virtexp.elphys.clampable import Clampable
-from cgp.virtexp.elphys.paceable import ap_stats_array, Paceable
-from cgp.rnumpy.rnumpy import r, RRuntimeError, py2ri, rcopy
+from cgp.virtexp.elphys.paceable import Paceable
+# Utilities
+from cgp.virtexp.elphys.paceable import ap_stats_array
 from cgp.utils.unstruct import unstruct
 from cgp.utils.failwith import failwithnanlikefirst
 
-# This requires that an ipcluster is already started, see
-# http://ipython.org/ipython-doc/stable/parallel/parallel_task.html#parallel-function-decorator
-# rc = Client()
-# lview = rc.load_balanced_view()
-try:
-    r.library("sensitivity")
-except RRuntimeError:
-    r.install_packages("sensitivity")
-    r.library("sensitivity")
-
-logging.basicConfig()
+# Sensitivity analysis package for R
+r.library("sensitivity")
+# Initialize caching
 mem = Memory("/tmp/sensitivity_beeler")
 
 class Model(Cellmlmodel, Clampable, Paceable, AttractorMixin):
     """Mix and match virtual experiments."""
     pass
 
+# Wrap CellML model and adjust parameter names to conform with pacing protocol
 m = Model("/beeler_reuter_1977", rename=dict(
     p=dict(IstimPeriod="stim_period", IstimAmplitude="stim_amplitude", 
     IstimPulseDuration="stim_duration")), 
     reltol=1e-10, maxsteps=1e6, chunksize=100000)
 m.pr.IstimStart = 0
 
-r.set_seed(20120221)
-
+# Will sample within plus/minus 50% of  all nonzero parameters
 factors = [k for k in m.dtype.p.names if m.pr[k] != 0]
 
-#@failwithnanlikefirst
-def _phenotypes(par=None):
+@mem.cache
+@failwithnanlikefirst
+def phenotypes(par=None):
     """Aggregate phenotypes for sensitivity analysis."""
     with m.autorestore(_p=par):
         m.eq(tmax=1e4, tol=1e-3)
         _t, _y, stats = m.ap()
     return ap_stats_array(stats)
-
-phenotypes = mem.cache(_phenotypes)  # avoid name conflict in joblib
 
 phenotypes()  # initialize and cache default
 
@@ -64,23 +58,36 @@ def mat2par(mat):
         par[k] = mat[:, i]
     return par
 
-@ri.rternalize
-def apd50(mat):
-    """Scalar function for use with R's sensitivity::morris()."""
-    ph = np.concatenate([phenotypes(i) for i in mat2par(mat)])
-    # np.save("sbmat.npy", mat)
-    # np.save("sbpar.npy", mat2par(mat))
-    # np.save("sbph.npy", ph)
-    result = ph["apd50"]
-    result[np.isnan(result)] = 0
-    return py2ri(result)
+def scalar_pheno(field):
+    """Make a function to return a named field of the phenotype array."""
+    
+    @ri.rternalize
+    def fun(rmatrix):
+        """Scalar function for use with R's sensitivity::morris()."""
+        ph = np.concatenate([phenotypes(i) for i in mat2par(rmatrix)])
+        result = ph[field]
+        result[np.isnan(result)] = 0
+        return py2ri(result)
+    
+    return fun
 
 if __name__ == "__main__":
-    binf = 0.5 * unstruct(m.pr[factors]).squeeze()
-    bsup = 1.5 * unstruct(m.pr[factors]).squeeze()
-    morr = r.morris(apd50, factors=factors, r=2, design={"type": "oat", "levels": 10, "grid.jump": 5}, binf=binf, bsup=bsup)
-    print morr
-#    r["morr"] = morr
-#    r.save("morr", file="morr.RData")
-#    r.str(morr)
-#    r.tell(morr)
+    # Sensitivity analysis
+    baseline = unstruct(m.pr[factors])
+    lower = 0.5 * baseline
+    upper = 1.5 * baseline
+    result = dict()
+    for field in "appeak", "apd90", "ctpeak", "ctbase", "ctd90":
+        r.set_seed(20120221)  # repeatable random sampling
+        result[field] = r.morris(scalar_pheno(field), factors=factors, r=2, 
+            design={"type": "oat", "levels": 10, "grid.jump": 5}, 
+            binf=lower, bsup=upper)
+    # Print and visualize results
+    r.png("sensitivity.png", width=1024, height=768, pointsize=24)
+    r.par(mfrow=(2, 3))  # multiple figures in two rows, three columns
+    for k, v in result.items():
+        print "===================================================="
+        print k
+        print v
+        r.plot(v, log="y", main=k)
+    r.dev_off()  # close image file
