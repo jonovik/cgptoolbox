f963df7d53b85c191455a609f9173dcaa267e27d
Jon Olav Vik <jonovik@gmail.com>
2012-02-28 19:30:15 +0100

    URL routing and GET arguments working. Return JSON.

--- cgp/examples/sensitivity_service/__main__.py
+++ cgp/examples/sensitivity_service/__main__.py
@@ -1,7 +1,28 @@
 """
 Web service for sensitivity analysis of CellML models.
 
+Usage example:
 
+http://localhost:8080/sensitivity/11df840d0150d34c9716cd4cbdd164c8/bondarenko_szigeti_bett_kim_rasmusson_2004_apical/protocol/statistic?par=g_Na+Nao
+
+In general:
+
+http://localhost:8080/sensitivity/<exposure>/<workspace>/<protocol>/<statistic>?par=p1+p2
+
+will analyse the sensitivity with respect to parameters p1 and p2
+of a <statistic> that characterizes the outcome of an experimental <protocol>
+applied to a CellML model identified by <exposure>/<workspace> at cellml.org.
+
+Parameters can be passed by GET (shown above) or POST (suitable for long 
+parameter lists).
+
+Valid formats for the query string for the parameter list are:
+par=p1+p2          space-delimited
+TODO: further formats
+par=p1&par=p2      "SELECT multiple" HTML query
+par=["p1", "p2"]   JSON, Python
+
+TODO:
 
 1. Dropdown list of all CellML models.
 1a. Restrict list to CellML models that have V.
@@ -13,36 +34,59 @@ Web service for sensitivity analysis of CellML models.
 7. AJAX instead of re-submitting on every input.
 """
 
-import webbrowser
-import urllib
+import cgi
+import json
 
 import bottle
-from bottle import route, run, view
-from cgp.physmod.cellmlmodel import get_all_workspaces, Cellmlmodel
-from joblib.memory import Memory
-from cgp.utils.rec2dict import rec2dict
-
-mem = Memory("/tmp/sensitivity")
-
-get_all_workspaces = mem.cache(get_all_workspaces)
+from bottle import route, run, view, request
+from ast import literal_eval
+from cgp.physmod.cellmlmodel import Cellmlmodel
+from cgp.virtexp.elphys.paceable import Paceable
+from cgp.utils.unstruct import unstruct
 
 @route("/")
 def index():
-    return "<pre>{}</pre>".format(__doc__)
+    """Usage instructions."""
+    return "<pre>{}</pre>".format(cgi.escape(__doc__))
 
-@route("/sensitivity")
-@view("sensitivity")
-def sensitivity():
-    # TODO: use workspace as identifier, get_latest_exposure
-    d = dict(workspaces=get_all_workspaces())
-    d["path"], _ = urllib.splitquery(bottle.request.url)
-    d["query"] = bottle.request.query
+class Model(Cellmlmodel, Paceable):
+    """CellML model wrapper with phenotyping mixed in."""
+    pass
+
+@route("/sensitivity/<exposure>/<workspace>/<protocol>/<statistic>")
+def sensitivity(exposure, workspace, protocol, statistic):
+    """
+    Sensitivity analysis.
+    
+    TODO: Make exposure optional.
+    """
+    par = request.params.par.split()  # space-delimited string
+    # par = json.loads(request.params.par)  # JSON or Python
+    # par = request.params.getall("par")  # HTML multiple SELECT
+    par = [str(i) for i in par]  # Numpy cannot handle Unicode names
     try:
-        d["model"] = Cellmlmodel("/fitzhugh_1961")
-        # d["model"] = Cellmlmodel(bottle.request.query.workspace)
-    except ZeroDivisionError:
-        d["model"] = None
-    return d
+        m = Model(exposure + "/" + workspace, maxsteps=1e6, chunksize=1e5, reltol=1e-8)
+    except IOError, exc:
+        return "<pre>{}</pre>".format(exc)
+    baseline = unstruct(m.pr[par]).squeeze()
+    lower = 0.5 * baseline
+    upper = 1.5 * baseline
+    return dict(lower=lower.tolist(), upper=upper.tolist())
+    #r.set_seed(request.params.seed)
+    #return r.morris(scalar_pheno(statistic), factors=par, r=2, design={"type": "oat", "levels": 10, "grid.jump": 5, binf=lower, bsup=upper})
 
-# webbrowser.open("http://localhost:8080/sensitivity")
+#@route("/sensitivity")
+#@view("sensitivity")
+#def sensitivity():
+#    # TODO: use workspace as identifier, get_latest_exposure
+#    d = dict(workspaces=get_all_workspaces())
+#    d["path"], _ = urllib.splitquery(bottle.request.url)
+#    d["query"] = bottle.request.query
+#    try:
+#        d["model"] = Cellmlmodel("/fitzhugh_1961")
+#        # d["model"] = Cellmlmodel(bottle.request.query.workspace)
+#    except ZeroDivisionError:
+#        d["model"] = None
+#    return d
+
 bottle.run(host='localhost', port=8080, debug=True, reloader=True)
