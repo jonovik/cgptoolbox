da387fc8435332732be9eb77636924c25ad0b228
Jon Olav Vik <jonovik@gmail.com>
2012-03-01 09:25:07 +0100

    Pickling Cvodeint works, even for (Cellmlmodel, Paceable).

--- cgp/examples/test_pickle.py
+++ cgp/examples/test_pickle.py
@@ -5,8 +5,19 @@ from cgp.virtexp.elphys.paceable import Paceable
 class Model(Cellmlmodel, Paceable):
     pass
 
-m = Model()
+m = Model(exposure_workspace="c7f7ced1e002d9f0af1b56b15a873736/tentusscher_noble_noble_panfilov_2004_a",
+        rename={"y": {"Na_i": "Nai", "Ca_i": "Cai", "K_i": "Ki"}, "p": {
+            "IstimStart": "stim_start", 
+            "IstimEnd": "stim_end", 
+            "IstimAmplitude": "stim_amplitude", 
+            "IstimPeriod": "stim_period", 
+            "IstimPulseDuration": "stim_duration"
+        }}, use_cython=True)
 
-print pickle.dumps(m)
+print m.__class__
+
+s = pickle.dumps(m)
+n = pickle.loads(s)
 
 print m
+print n
