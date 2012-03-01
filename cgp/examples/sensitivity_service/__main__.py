75e8c7723dc349950ad71bce4aaf8b2695d8a9d0
Jon Olav Vik <jonovik@gmail.com>
2012-03-01 16:51:01 +0100

    About to refactor bridge to R.

--- cgp/examples/sensitivity_service/__main__.py
+++ cgp/examples/sensitivity_service/__main__.py
@@ -77,8 +77,8 @@ class Model(Cellmlmodel, Paceable, AttractorMixin):
 mem.clear()
 
 @failwithnanlikefirst
-@mem.cache(ignore=["m"])
-def phenotypes(m, modelname, par=None):
+@mem.cache()
+def phenotypes(m, par=None):
     """Aggregate phenotypes for sensitivity analysis."""
     with m.autorestore(_p=par):
         m.eq(tmax=1e4, tol=1e-3)
@@ -107,18 +107,25 @@ def scalar_pheno(field, m, factors):
     
     >>> m = Model("11df840d0150d34c9716cd4cbdd164c8/bondarenko_szigeti_bett_kim_rasmusson_2004_apical")
     >>> factors = "Cm", "Vmyo"
-    >>> phenotypes(m, m.name)
+    >>> print "Result:", repr(phenotypes(m))
+    Result: ...
+    rec.array([ (0.00013341746414141653, -82.4202, -82.42006658253585, 71.43, nan, 6.086027625230692, nan, nan, nan, 0.0, 0.115001, 0.115001, 0.0, nan, 0.0, 0.0, 0.0, 0.0)], 
+          dtype=[('apamp', '<f8'), ('apbase', '<f8'), ('appeak', '<f8'), ('apttp', '<f8'), ('apdecayrate', '<f8'), ('apd25', '<f8'), ('apd50', '<f8'), ('apd75', '<f8'), ('apd90', '<f8'), ('ctamp', '<f8'), ('ctbase', '<f8'), ('ctpeak', '<f8'), ('ctttp', '<f8'), ('ctdecayrate', '<f8'), ('ctd25', '<f8'), ('ctd50', '<f8'), ('ctd75', '<f8'), ('ctd90', '<f8')])
     >>> fun = scalar_pheno("apd90", m, factors)
     >>> rmatrix = r.matrix(m.pr.item(), nrow=1)
     >>> type(rmatrix)
+    <class 'cgp.rnumpy.rnumpy.RArray'>
     >>> mat2par(rmatrix, m, factors)
+    rec.array([ (1.0, 2.584e-05, 1.2e-07, 2.098e-06, 1.485e-09, 0.0001534, 5400.0, 140000.0, 1800.0, 8.314, 298.0, 96.5, 20.0, 100000.0, 71.43, 0.5, -80.0, 50.0, 15000.0, 0.238, 800.0, 0.00237, 3.2e-05, 0.0327, 0.0196, 4.5, 20.0, 1.74e-05, 8.0, 0.45, 0.5, 70.0, 140.0, 7.0, 0.006075, 0.07125, 0.00405, 0.965, 0.009, 0.0008, 3.0, 4.0, 63.0, 0.1729, 0.0005, 0.23324, 20.0, 1.0, 0.5, 292.8, 87500.0, 1380.0, 0.1, 0.35, 0.000367, 13.0, 0.0026, 0.4067, 0.0, 0.00575, 0.16, 0.05, 0.078, 0.036778, 0.023761, 0.88, 21000.0, 1500.0, 10.0, -40.0, 10.0, 1.0009103049457284, 0.0)], 
+          dtype=[('Cm', '<f8'), ('Vmyo', '<f8'), ('VJSR', '<f8'), ('VNSR', '<f8'), ('Vss', '<f8'), ('Acap', '<f8'), ('Ko', '<f8'), ('Nao', '<f8'), ('Cao', '<f8'), ('R', '<f8'), ('T', '<f8'), ('F', '<f8'), ('stim_start', '<f8'), ('stim_end', '<f8'), ('stim_period', '<f8'), ('stim_duration', '<f8'), ('stim_amplitude', '<f8'), ('CMDN_tot', '<f8'), ('CSQN_tot', '<f8'), ('Km_CMDN', '<f8'), ('Km_CSQN', '<f8'), ('k_plus_htrpn', '<f8'), ('k_minus_htrpn', '<f8'), ('k_plus_ltrpn', '<f8'), ('k_minus_ltrpn', '<f8'), ('v1', '<f8'), ('tau_tr', '<f8'), ('v2', '<f8'), ('tau_xfer', '<f8'), ('v3', '<f8'), ('Km_up', '<f8'), ('LTRPN_tot', '<f8'), ('HTRPN_tot', '<f8'), ('i_CaL_max', '<f8'), ('k_plus_a', '<f8'), ('k_minus_a', '<f8'), ('k_plus_b', '<f8'), ('k_minus_b', '<f8'), ('k_plus_c', '<f8'), ('k_minus_c', '<f8'), ('m', '<f8'), ('n', '<f8'), ('E_CaL', '<f8'), ('g_CaL', '<f8'), ('Kpcb', '<f8'), ('Kpc_max', '<f8'), ('Kpc_half', '<f8'), ('i_pCa_max', '<f8'), ('Km_pCa', '<f8'), ('k_NaCa', '<f8'), ('K_mNa', '<f8'), ('K_mCa', '<f8'), ('k_sat', '<f8'), ('eta', '<f8'), ('g_Cab', '<f8'), ('g_Na', '<f8'), ('g_Nab', '<f8'), ('g_Kto_f', '<f8'), ('g_Kto_s', '<f8'), ('g_Ks', '<f8'), ('g_Kur', '<f8'), ('g_Kss', '<f8'), ('g_Kr', '<f8'), ('kb', '<f8'), ('kf', '<f8'), ('i_NaK_max', '<f8'), ('Km_Nai', '<f8'), ('Km_Ko', '<f8'), ('g_ClCa', '<f8'), ('E_Cl', '<f8'), ('Km_Cl', '<f8'), ('sigma', '<f8'), ('empty__72', '<f8')])
     >>> fun(r.matrix(m.pr.item(), nrow=1))
     """
     
     @ri.rternalize
     def fun(rmatrix):
         """Scalar function for use with R's sensitivity::morris()."""
-        ph = np.concatenate([phenotypes(m, m.name, i) for i in mat2par(rmatrix, m, factors)])
+        r.str(rmatrix)
+        ph = np.concatenate([phenotypes(m, i) for i in mat2par(rmatrix, m, factors)])
         return py2ri(ph[field])
     
     return fun
@@ -127,13 +134,22 @@ def _sensitivity(exposure, workspace, protocol, statistic, par=None, seed=None):
     """
     Sensitivity analysis.
     
+    Callback from R.
+    
+    >>> r("fun <- function(func, x) func(x)")
+    RClosure with name <return value from eval>:
+    <R function>
+    >>> r.fun(r.sum, range(5))
+    array([10])
+    (R-style, sealed)
+    
     >>> _sensitivity("11df840d0150d34c9716cd4cbdd164c8", "bondarenko_szigeti_bett_kim_rasmusson_2004_apical", "protocol", "apd90", ("g_Na", "Nao"), seed=1)
 
     TODO: Make optional arguments of exposure, lower, upper, etc.
     TODO: Accept json dict of model_kwargs, morris_kwargs
     """
     m = Model(exposure + "/" + workspace, maxsteps=1e6, chunksize=1e5, reltol=1e-8)
-    phenotypes(m, m.name)  # initialize and cache default
+    phenotypes(m)  # initialize and cache default
     par = [str(i) for i in par]  # Numpy cannot handle Unicode names
     if not par:
         # Default: sample within plus/minus 50% of all nonzero parameters
@@ -144,11 +160,7 @@ def _sensitivity(exposure, workspace, protocol, statistic, par=None, seed=None):
     if seed is not None:
         r.set_seed(seed)
     
-    r("fun <- function(func, x) func(x)")
-    raise Exception(str(r.fun(r.sum, baseline)))
-
-    
-    fun = scalar_pheno(m, statistic)
+    fun = scalar_pheno(m, statistic, par)
     r.as_vector(baseline)
     return r.morris(fun, factors=par, r=2, design={"type": "oat", "levels": 10, "grid.jump": 5}, binf=lower, bsup=upper)
 
