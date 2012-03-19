5884d053372025e56d939ed5af6a3f20a75afea7
Jon Olav Vik <jonovik@gmail.com>
2012-03-19 10:58:01 +0100

    Use (exposure, workspace, ...) identifiers for sensitivity examples.

--- cgp/examples/sensitivity_service/__main__.py
+++ cgp/examples/sensitivity_service/__main__.py
@@ -3,6 +3,10 @@ Web service for sensitivity analysis of CellML models.
 
 Usage example:
 
+wget http://localhost:8081 -O -
+
+wget http://localhost:8081/sensitivity/ap/ctd50/beeler_reuter_1977?par=g_Na+E_Na -O -
+
 http://localhost:8080/sensitivity/11df840d0150d34c9716cd4cbdd164c8/bondarenko_szigeti_bett_kim_rasmusson_2004_apical/protocol/statistic?par=g_Na+Nao
 
 In general:
@@ -44,6 +48,26 @@ from cgp.physmod.cellmlmodel import Cellmlmodel
 from cgp.virtexp.elphys.paceable import Paceable
 from cgp.utils.unstruct import unstruct
 
+# Numerics
+import numpy as np
+# Bridge to the R statistical software
+import rpy2.rinterface as ri
+from cgp.rnumpy.rnumpy import r, py2ri
+
+r.library("sensitivity")
+
+def scalar_pheno(field):
+    """Make a function to return a named field of the phenotype array."""
+    
+    @ri.rternalize
+    def fun(rmatrix):
+        """Scalar function for use with R's sensitivity::morris()."""
+        ph = np.concatenate([phenotypes(i) for i in mat2par(rmatrix)])
+        return py2ri(ph[field])
+    
+    return fun
+
+
 @route("/")
 def index():
     """Usage instructions."""
@@ -53,8 +77,8 @@ class Model(Cellmlmodel, Paceable):
     """CellML model wrapper with phenotyping mixed in."""
     pass
 
-@route("/sensitivity/<exposure>/<workspace>/<protocol>/<statistic>")
-def sensitivity(exposure, workspace, protocol, statistic):
+@route
+def sensitivity(protocol, statistic, workspace, exposure="", changeset="", variant=""):
     """
     Sensitivity analysis.
     
@@ -65,15 +89,15 @@ def sensitivity(exposure, workspace, protocol, statistic):
     # par = request.params.getall("par")  # HTML multiple SELECT
     par = [str(i) for i in par]  # Numpy cannot handle Unicode names
     try:
-        m = Model(exposure + "/" + workspace, maxsteps=1e6, chunksize=1e5, reltol=1e-8)
+        m = Model(workspace, exposure, changeset, variant, maxsteps=1e6, chunksize=1e5, reltol=1e-8)
     except IOError, exc:
         return "<pre>{}</pre>".format(exc)
     baseline = unstruct(m.pr[par]).squeeze()
     lower = 0.5 * baseline
     upper = 1.5 * baseline
-    return dict(lower=lower.tolist(), upper=upper.tolist())
-    #r.set_seed(request.params.seed)
-    #return r.morris(scalar_pheno(statistic), factors=par, r=2, design={"type": "oat", "levels": 10, "grid.jump": 5, binf=lower, bsup=upper})
+    # return dict(lower=lower.tolist(), upper=upper.tolist())
+    r.set_seed(request.params.seed or 1)
+    return r.morris(scalar_pheno(statistic), factors=par, r=2, design={"type": "oat", "levels": 10, "grid.jump": 5}, binf=lower, bsup=upper)
 
 #@route("/sensitivity")
 #@view("sensitivity")
@@ -89,4 +113,5 @@ def sensitivity(exposure, workspace, protocol, statistic):
 #        d["model"] = None
 #    return d
 
-bottle.run(host='localhost', port=8080, debug=True, reloader=True)
+if __name__ == "__main__":
+    bottle.run(debug=True, reloader=True, port=8081)
