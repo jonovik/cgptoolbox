683d83d920f986242e50c3327b9ee787c34013d4
argju <arne.gjuvsland@gmail.com>
2012-12-07 20:56:50 +0100

    Basic setup.py file (see http://docs.python.org/2/distutils/setupscript.html)

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cgptoolbox',
+      version='0.1',
+      description='A toolbox for causally cohesive genotype-phenotype modeling',
+      maintainer='Jon Olav Vik',
+      maintainer_email='jonovik@gmail.com',
+      url='http://arken.umb.no/~jonvi/cgptoolbox/'
+      download_url='https://github.com/jonovik/cgptoolbox',
+      packages = find_packages(),
+     )
