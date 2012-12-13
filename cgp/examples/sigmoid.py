871c1609f6ae591c816d53e339ac05f3b51349d3
Jon Olav Vik <jonovik@gmail.com>
2012-12-13 09:26:20 +0100

    Fix whitespace for sigmoid example. The example is not finished yet.

--- cgp/examples/sigmoid.py
+++ cgp/examples/sigmoid.py
@@ -4,44 +4,42 @@ Basic example of cGP study using sigmoid model of gene regulatory networks.
 # pylint: disable=W0621
 
 import numpy as np
-from matplotlib import pyplot as plt
 
-from cgp.utils.placevalue import Placevalue	
+from cgp.utils.placevalue import Placevalue
 from cgp.gt.gt2par import geno2par_diploid
 from cgp.sigmoidmodels.sigmoid_cvode import Sigmoidmodel
 from cgp.phenotyping.attractor import AttractorMixin
-from cgp.utils.recfun import cbind, restruct
 
-# ode model  
-class Model(Sigmoidmodel,AttractorMixin):
-  pass
+class Model(Sigmoidmodel, AttractorMixin):
+    """Combines a sigmoid model with the ability to find equilibria."""
+    pass
 
 model = Model()
 
 
 # Allelic parameter values sampled as defined in Gjuvsland et al. (2007)
 hetpar = model.sample_hetpar()
-relvar = np.zeros((3,18))
-relvar[0,0:6] = 1
-relvar[1,6:12] = 1
-relvar[2,12:18] = 1
-names = ['Gene1','Gene2','Gene3']
+relvar = np.zeros((3, 18))
+relvar[0, 0:6] = 1
+relvar[1, 6:12] = 1
+relvar[2, 12:18] = 1
+names = ['Gene1', 'Gene2', 'Gene3']
 
 # Enumerate genotypes
-genotypes = Placevalue([3,3,3],names=names,msd_first=False)
+genotypes = Placevalue([3, 3, 3], names=names, msd_first=False)
 
 
-def gt2par(gt,hetpar,relvar):
+def gt2par(gt, hetpar, relvar):
     """Genotype-to-parameter mapping."""
-    return geno2par_diploid(gt,hetpar,relvar)
+    return geno2par_diploid(gt, hetpar, relvar)
 
 def par2ph(par):
     """Parameter-to-phenotype mapping."""
-    model._ReInit_if_required(y=np.array([0.0,0.0,0.0,0.0,0.0,0.0]))
+    model._ReInit_if_required(y=np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
     model.pr[:] = par
-    t, y = model.eq(tmax=100,tol=1e-6)
-    y = np.array([y[0]+y[1],y[2]+y[3],y[4]+y[5]])
-    y.dtype=[("Y1", float), ("Y2", float), ("Y3", float)]
+    t, y = model.eq(tmax=100, tol=1e-6)
+    y = np.array([y[0] + y[1], y[2] + y[3], y[4] + y[5]])
+    y.dtype = [("Y1", float), ("Y2", float), ("Y3", float)]
     return y.view(np.recarray)
 
 def cgpstudy():
@@ -60,9 +58,6 @@ def cgpstudy():
     gt = np.array(genotypes)
     par = cat([gt2par(list(g), hetpar, absvar) for g in gt])
     ph = cat([par2ph(p) for p in par])
-     
-
-
 
 if __name__ == "__main__":
     cgpstudy()
