5116167d96ad1628e95aab33e5ec6383d0e7e713
Jon Olav Vik <jonovik@gmail.com>
2012-12-13 08:26:07 +0100

    Convert tabs to spaces. Remove unused imports. Silence pylint warnings (to be dealt with later).

--- cgp/sigmoidmodels/sigmoid_cvode.py
+++ cgp/sigmoidmodels/sigmoid_cvode.py
@@ -22,12 +22,13 @@ References:
 
 """
 
+# TODO: fix these later
+# pylint:disable=C0301,C0324,W0105,W0102,C0322
+
 from __future__ import division
 from ..cvodeint.namedcvodeint import Namedcvodeint
-from ..sigmoidmodels.doseresponse import Hill, R_logic, nonmono
-from ..utils.rec2dict import dict2rec
+from ..sigmoidmodels.doseresponse import Hill, R_logic
 import numpy as np
-            
 
 class Sigmoidmodel(Namedcvodeint):
     """
@@ -41,25 +42,25 @@ class Sigmoidmodel(Namedcvodeint):
 
     #ode parameter (production rates, decay rates, shape of gene regulation funciton
     _origpar = [ 
-      ('alpha1',  150, 100),
-      ('theta11', 20,  20),
-      ('p11',     5.5, 9),
-      ('theta12', 20,  20),
-      ('p12',     5.5, 9),
-      ('gamma1',  10,  0),
-      ('alpha2',  150, 100),
-      ('theta21', 20,  20),
-      ('p21',     5.5, 9),
-      ('theta22', 20,  20),
-      ('p22',     5.5, 9),
-      ('gamma2',  10,  0),
-      ('alpha3',  150, 100),
-      ('theta31', 20,  20),
-      ('p31',     5.5, 9),
-      ('theta32', 20,  20),
-      ('p32',     5.5, 9),
-      ('gamma3',  10,  0)
-      ]
+        ('alpha1',  150, 100),
+        ('theta11', 20,  20),
+        ('p11',     5.5, 9),
+        ('theta12', 20,  20),
+        ('p12',     5.5, 9),
+        ('gamma1',  10,  0),
+        ('alpha2',  150, 100),
+        ('theta21', 20,  20),
+        ('p21',     5.5, 9),
+        ('theta22', 20,  20),
+        ('p22',     5.5, 9),
+        ('gamma2',  10,  0),
+        ('alpha3',  150, 100),
+        ('theta31', 20,  20),
+        ('p31',     5.5, 9),
+        ('theta32', 20,  20),
+        ('p32',     5.5, 9),
+        ('gamma3',  10,  0)
+        ]
 
     par_names, par_mean, par_span = zip(*_origpar)
     """
@@ -76,106 +77,96 @@ class Sigmoidmodel(Namedcvodeint):
     _dtype = np.dtype([(_name,'float',(2,)) for _name in par_names])
     p = par_mean.flatten().view(_dtype,np.recarray)
 
-    
     def __init__(self, t=[0,1],y=np.array([0.0,0.0,0.0,0.0,0.0,0.0]),p=p,adjmotif=adjmotif):
-	"""
-	Constructor
-	"""
-	self.adjmotif = adjmotif
-	super(Sigmoidmodel, self).__init__(self.equations_diploid_adjacency, t, y.view(float), p=p)
-	
+        """
+        Constructor
+        """
+        self.adjmotif = adjmotif
+        super(Sigmoidmodel, self).__init__(self.equations_diploid_adjacency, t, y.view(float), p=p)
+        
     def equations_diploid_adjacency(self,t,y,ydot,fdata):
-	"""cvode compliant ode rate function for a diploid model involving three loci X1,X2,X3
-	
-	This is a general model where each gene can be regulated by two of the three genes. 
-	The actual motif is controlled by the adjacency matrix adjmotif
-	
-	:TODO: for JO use adjmotifs as input to networkX to create plot of directed graph 
-	
-	"""
-
-	Y = np.nan*np.ones((4,))
-	Y[0]=y[0]+y[1]   # sum of x11 and x12
-	Y[1]=y[2]+y[3]   # sum of x21 and x22
-	Y[2]=y[4]+y[5]   # sum of x31 and x32
-	Y[3]=np.nan	     # dummy variable used when there is no regulator
-	
-	regulators, grfmotifs = self.adjacency_to_grf()
-	  
-	# computing the derivatives
-	p = self.p
-	ydot[0] = p.alpha1[0,0]*R_logic(Hill(Y[regulators[0,0]],p.theta11[0,0],p.p11[0,0]),Hill(Y[regulators[0,1]],p.theta12[0,0],p.p12[0,0]),grfmotifs[0])  -p.gamma1[0,0]*y[0]
-	ydot[1] = p.alpha1[0,1]*R_logic(Hill(Y[regulators[0,0]],p.theta11[0,1],p.p11[0,1]),Hill(Y[regulators[0,1]],p.theta12[0,1],p.p12[0,1]),grfmotifs[0])  -p.gamma1[0,1]*y[1]
-	ydot[2] = p.alpha2[0,0]*R_logic(Hill(Y[regulators[1,0]],p.theta21[0,0],p.p21[0,0]),Hill(Y[regulators[1,1]],p.theta22[0,0],p.p22[0,0]),grfmotifs[1])  -p.gamma2[0,0]*y[2]
-	ydot[3] = p.alpha2[0,1]*R_logic(Hill(Y[regulators[1,0]],p.theta21[0,1],p.p21[0,1]),Hill(Y[regulators[1,1]],p.theta22[0,1],p.p22[0,1]),grfmotifs[1])  -p.gamma2[0,1]*y[3]
-	ydot[4] = p.alpha3[0,0]*R_logic(Hill(Y[regulators[2,0]],p.theta31[0,0],p.p31[0,0]),Hill(Y[regulators[2,1]],p.theta32[0,0],p.p32[0,0]),grfmotifs[2])  -p.gamma3[0,0]*y[4]
-	ydot[5] = p.alpha3[0,1]*R_logic(Hill(Y[regulators[2,0]],p.theta31[0,1],p.p31[0,1]),Hill(Y[regulators[2,1]],p.theta32[0,1],p.p32[0,1]),grfmotifs[2])  -p.gamma3[0,1]*y[5]
+        """cvode compliant ode rate function for a diploid model involving three loci X1,X2,X3
+        
+        This is a general model where each gene can be regulated by two of the three genes. 
+        The actual motif is controlled by the adjacency matrix adjmotif
+        
+        :TODO: for JO use adjmotifs as input to networkX to create plot of directed graph 
+        
+        """
+        
+        Y = np.nan*np.ones((4,))
+        Y[0]=y[0]+y[1]   # sum of x11 and x12
+        Y[1]=y[2]+y[3]   # sum of x21 and x22
+        Y[2]=y[4]+y[5]   # sum of x31 and x32
+        Y[3]=np.nan	     # dummy variable used when there is no regulator
+        
+        regulators, grfmotifs = self.adjacency_to_grf()
+          
+        # computing the derivatives
+        p = self.p
+        ydot[0] = p.alpha1[0,0]*R_logic(Hill(Y[regulators[0,0]],p.theta11[0,0],p.p11[0,0]),Hill(Y[regulators[0,1]],p.theta12[0,0],p.p12[0,0]),grfmotifs[0])  -p.gamma1[0,0]*y[0]
+        ydot[1] = p.alpha1[0,1]*R_logic(Hill(Y[regulators[0,0]],p.theta11[0,1],p.p11[0,1]),Hill(Y[regulators[0,1]],p.theta12[0,1],p.p12[0,1]),grfmotifs[0])  -p.gamma1[0,1]*y[1]
+        ydot[2] = p.alpha2[0,0]*R_logic(Hill(Y[regulators[1,0]],p.theta21[0,0],p.p21[0,0]),Hill(Y[regulators[1,1]],p.theta22[0,0],p.p22[0,0]),grfmotifs[1])  -p.gamma2[0,0]*y[2]
+        ydot[3] = p.alpha2[0,1]*R_logic(Hill(Y[regulators[1,0]],p.theta21[0,1],p.p21[0,1]),Hill(Y[regulators[1,1]],p.theta22[0,1],p.p22[0,1]),grfmotifs[1])  -p.gamma2[0,1]*y[3]
+        ydot[4] = p.alpha3[0,0]*R_logic(Hill(Y[regulators[2,0]],p.theta31[0,0],p.p31[0,0]),Hill(Y[regulators[2,1]],p.theta32[0,0],p.p32[0,0]),grfmotifs[2])  -p.gamma3[0,0]*y[4]
+        ydot[5] = p.alpha3[0,1]*R_logic(Hill(Y[regulators[2,0]],p.theta31[0,1],p.p31[0,1]),Hill(Y[regulators[2,1]],p.theta32[0,1],p.p32[0,1]),grfmotifs[2])  -p.gamma3[0,1]*y[5]
 
     def adjacency_to_grf(self):
-	"""
-	Convert adjacency matrix of Sigmoidmodel instance to nested lists of 
-	regulators and gene regulation functions.
-	
-	:return regulators: 3-element list of indexes of regulator gene for each gene such 
-			    that gene i is a regulator of gene j iff regulators[j] contains i
-	:return grfmotifs:  3-element list of integers indicating the Boolean function 
-			    used in the gene regulation functions. 
-	
-	"""
-	adjmotif = self.adjmotif
-	regulators = np.nan*np.zeros((3,2))
-	grfmotifs = np.nan*np.zeros(3,)
-	
-	for i in range(3):
-	  if np.shape(adjmotif[i,:].nonzero()[0])==(0,): 	#no regulators
-	    regulators[i,:] = [3,3]	#no regulators
-	    grfmotifs[i] = 16 	#always on
-	  elif np.shape(adjmotif[i,:].nonzero()[0])==(1,):	#one regulator
-	    regulators[i,:]=[adjmotif[i,:].nonzero()[0][0],3] 
-	    if adjmotif[i,regulators[i,0]]==1:
-	      grfmotifs[i] = 5
-	    else:
-	      grfmotifs[i] = 7
-	  elif np.shape(adjmotif[i,:].nonzero()[0])==(2,):	#two regulators
-	      regulators[i,:]=adjmotif[i,:].nonzero()[0]
-	      if np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([1,1])):
-		grfmotifs[i] = 1
-	      elif np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([1,-1])):
-		grfmotifs[i] = 2
-	      elif np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([-1,1])):
-		grfmotifs[i] = 3
-	      elif np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([-1,-1])):
-		grfmotifs[i] = 4
-	      else:
-		raise Exception('Only -1 and 1 allowed in adjacency matrix')
-	  else: 
-	      raise Exception('Only 0,1 or 2 regulators allowed per gene')
-
-	return (regulators,grfmotifs)
+        """
+        Convert adjacency matrix of Sigmoidmodel instance to nested lists of 
+        regulators and gene regulation functions.
+        
+        :return regulators: 3-element list of indexes of regulator gene for each gene such 
+        		    that gene i is a regulator of gene j iff regulators[j] contains i
+        :return grfmotifs:  3-element list of integers indicating the Boolean function 
+        		    used in the gene regulation functions. 
+        
+        """
+        adjmotif = self.adjmotif
+        regulators = np.nan*np.zeros((3,2))
+        grfmotifs = np.nan*np.zeros(3,)
+        
+        for i in range(3):
+            if np.shape(adjmotif[i,:].nonzero()[0])==(0,): 	#no regulators
+                regulators[i,:] = [3,3]	#no regulators
+                grfmotifs[i] = 16 	#always on
+            elif np.shape(adjmotif[i,:].nonzero()[0])==(1,):	#one regulator
+                regulators[i,:]=[adjmotif[i,:].nonzero()[0][0],3] 
+                if adjmotif[i,regulators[i,0]]==1:
+                    grfmotifs[i] = 5
+                else:
+                    grfmotifs[i] = 7
+            elif np.shape(adjmotif[i,:].nonzero()[0])==(2,):	#two regulators
+                regulators[i,:]=adjmotif[i,:].nonzero()[0]
+                if np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([1,1])):
+                    grfmotifs[i] = 1
+                elif np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([1,-1])):
+                    grfmotifs[i] = 2
+                elif np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([-1,1])):
+                    grfmotifs[i] = 3
+                elif np.min(adjmotif[i,adjmotif[i,:].nonzero()[0]] == np.array([-1,-1])):
+                    grfmotifs[i] = 4
+                else:
+                    raise Exception('Only -1 and 1 allowed in adjacency matrix')
+            else: 
+                raise Exception('Only 0,1 or 2 regulators allowed per gene')
+        
+        return (regulators,grfmotifs)
 
     def sample_hetpar(self):
-	"""
-	Sample allelic parameter values uniformly with mean self.par_mean and span self.par_span
-	
-	:return hetpar: numpy recarray like self.p with parameter values for an all-loci heterozygot
-	
-	"""
-	low = self.par_mean - self.par_span/2
-	high = self.par_mean + self.par_span/2
-	hetpar = np.row_stack([np.random.uniform(size=(1,2), low=l, high=h) for l,h in zip(low,high)])
-	#hetpar = uni.rvs(size=self.par_mean.shape,loc=self.par_mean-self.par_span/2,scale=self.par_span)
-	hetpar = hetpar.flatten().view(self.p.dtype,np.recarray)
-	return hetpar
-   
-
-	
-
-    
-
+        """
+        Sample allelic parameter values uniformly with mean self.par_mean and span self.par_span
+        
+        :return hetpar: numpy recarray like self.p with parameter values for an all-loci heterozygot
+        
+        """
+        low = self.par_mean - self.par_span/2
+        high = self.par_mean + self.par_span/2
+        hetpar = np.row_stack([np.random.uniform(size=(1,2), low=l, high=h) for l,h in zip(low,high)])
+        #hetpar = uni.rvs(size=self.par_mean.shape,loc=self.par_mean-self.par_span/2,scale=self.par_span)
+        hetpar = hetpar.flatten().view(self.p.dtype,np.recarray)
+        return hetpar
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
-
-
-
